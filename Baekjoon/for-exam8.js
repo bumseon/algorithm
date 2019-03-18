b8398d72544e8a31981f140319dd2635cc54071d
bumseon <21mounter@naver.com>
2019-03-18 13:55:34 +0900

    fix the error

--- Baekjoon/for-exam8.js
+++ Baekjoon/for-exam8.js
@@ -5,21 +5,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.on('Month', function line(month){
-    const Month = month;
-    rl.close();
-});
-
-rl.on('Day', function line1(day){
-    const Day = day;
-    rl.close();
-});
-
-function solution(Month, Day){
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', function line(input){
+    const [month, day] = input.split(' ');
     let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
     let year = '2007';
-    let month = Month;
-    let day = Day;
     let dayOfWeek = week[new Date(year+'-'+month+'-'+day).getDay()];
     console.log(dayOfWeek);
-}
+    rl.close();
+});
+
+// function solution(Month, Day){
+//     let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+//     let year = '2007';
+//     let month = Month;
+//     let day = Day;
+//     let dayOfWeek = week[new Date(year+'-'+month+'-'+day).getDay()];
+//     return dayOfWeek;
+// }
+
+// console.log(solution(1, 1));
